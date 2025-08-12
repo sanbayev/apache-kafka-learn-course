2f7090cbb265cdc338355165296b7dffa2af4c9e
askar <sanbayevgit@gmail.com>
2025-08-12 16:06:19 +0500

    producre

---
+++ producer/src/main/java/org/example/ProducerDemo.java
@@ -0,0 +1,35 @@
+package org.example;
+
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class);
+
+    public static void main(String[] args) {
+
+        String bootstrapServers = "172.17.241.204:9092";
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+        ProducerRecord<String, String> producerRecord =
+                new ProducerRecord<>("first_topic", "hello world2");
+
+        producer.send(producerRecord);
+
+        producer.flush();
+        producer.close();
+    }
+}
